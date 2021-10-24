8189a1b2e5859c16a47ff7d4bd748024cf12ff34
Arish Khan <mohammadarishkhan503@gmail.com>
2021-10-24 19:39:32 +0530

    integrate category filter(wip)

---
+++ swap/Scene/Home/Cell/HomeCollectionViewCell.swift
@@ -0,0 +1,15 @@
+//
+//  HomeCollectionViewCell.swift
+//  swap
+//
+//  Created by Bigsur on 24/10/21.
+//
+
+import UIKit
+
+class HomeCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var categoryImageView: UIImageView!
+    @IBOutlet weak var categoryTitleLabel: UILabel!
+    
+    
+}
--- swap/Scene/Home/Controller/HomeViewController.swift
+++ swap/Scene/Home/Controller/HomeViewController.swift
@@ -8,6 +8,9 @@
 import UIKit
 
 class HomeViewController: UIViewController {
+    
+    private var categoryImages: [String] = ["Electronics", "Fashion", "Furniture", "HomeAppliances", "Toys&Games", "Others"]
+    private var categoryImagesTitle: [String] = ["Electronics", "Fashion", "Furniture", "Home Appliances", "Toys & Games", "Others"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,4 +24,29 @@ class HomeViewController: UIViewController {
         performSegue(withIdentifier: "AddItemViewControllerIdentifier", sender: self)
     }
 
+}
+ 
+extension HomeViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return categoryImages.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "HomeCollectionViewCellIdentifier", for: indexPath) as? HomeCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        if let image = UIImage(named: categoryImages[indexPath.row]) {
+            cell.categoryImageView.image = image
+        }
+         
+        cell.categoryTitleLabel.text = categoryImagesTitle[indexPath.row]
+        
+        return cell
+        
+        
+    }
+    
+    
+    
 }
